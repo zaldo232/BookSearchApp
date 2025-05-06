40dca475c6d19abf3cad64f52e993896bb69dff5
zaldo232 <zaldo232@naver.com>
2025-05-07 01:28:40 +0900

    Initial commit

---
+++ BookSearchApp/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace BookSearchApp.Models;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string[] Authors { get; set; }
+    public string Publisher { get; set; }
+    public string Thumbnail { get; set; }
+    public string Contents { get; set; }
+    public int Price { get; set; }
+}
+
+public class BookSearchResult
+{
+    public List<Book> Documents { get; set; }
+}
---
+++ BookSearchApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using BookSearchApp.Models;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace BookSearchApp.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    private readonly HttpClient _httpClient;
+
+    [ObservableProperty]
+    private string query;
+
+    [ObservableProperty]
+    private ObservableCollection<Book> books = new();
+
+    public MainViewModel()
+    {
+        _httpClient = new HttpClient();
+        _httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("KakaoAK", "5200b4fa592d9f490f8b2be6f3501ab4");
+    }
+
+    [RelayCommand]
+    public async Task SearchAsync()
+    {
+        if (string.IsNullOrWhiteSpace(Query)) return;
+
+        var url = $"https://dapi.kakao.com/v3/search/book?query={Uri.EscapeDataString(Query)}";
+        var response = await _httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<BookSearchResult>(json,
+            new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                }
+            );
+
+            // null 체크 추가
+            if (result?.Documents != null)
+            {
+                Books = new ObservableCollection<Book>(result.Documents);
+            }
+            else
+            {
+                Books = new ObservableCollection<Book>();
+            }
+        }
+
+    }
+}
